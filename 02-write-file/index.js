e0b0f2e851f700f0cd0d9bc970f2969c9a8e24c2
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-05-18 15:06:20 +0300

    feat: passed 2 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
-fs.writeFile('./02-write-file/text.txt', ' ', (err) => {
-  if(err) {
-    throw err;
+const fileName = fs.createWriteStream(path.join('./02-write-file/text.txt'));
+
+process.stdout.write('Hello, u can write your text here!');
+process.stdin.on('data', (message) => {
+  if(message.toString() === 'exit') {
+    closeConsole();
+  } else {
+    fileName.write(message);
   }
 });
+
+function closeConsole() {
+  process.stdout.write('Close write place');
+  process.exit();
+}
+
+process.on('SIGINT', closeConsole);
