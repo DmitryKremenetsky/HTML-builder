5ec93d4d472ad6dc9e4f47b33acc377243943bf4
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-05-20 22:04:03 +0300

    feat: add new task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+ 
+const createCopyDir = path.join(__dirname, 'copy-files');
+const addFolderCopy = path.join(__dirname, 'files');
 
+fs.promises.mkdir(createCopyDir, {recursive: true});
+
+fs.readdir(addFolderCopy, (err, files) => {
+  if (err) {
+    console.log(err);
+  } 
+
+  for (let i = 0; i < files.length; i++) {
+    fs.copyFile(addFolderCopy + '/' + files[i], createCopyDir + '/' + files[i], err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
