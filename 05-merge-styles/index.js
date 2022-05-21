b939bb1aeec51e811bd94ade3a80fac297e5c4a4
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2022-05-21 14:19:40 +0300

    feat: add task 5 done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const createBundel = path.join(__dirname, 'project-dist', 'bundel.css');
+const styleComponent = path.join(__dirname, 'styles');
+let writeStream = fs.createWriteStream(createBundel);
+
+fs.readdir(styleComponent, (err, files) => {
+  files.forEach(file => {
+    if (path.extname(file, err) == '.css') {
+      if (err) {
+        console.log(err); 
+      } else {
+        const newBundel = path.join(__dirname, 'styles', file);
+        const stream = fs.createReadStream(newBundel);
+        stream.on('data', (chunk , err) => {
+          if (err) {
+            console.log(err);
+          }
+          writeStream.write(chunk);
+        });
+      }
+    }
+  });
+});
